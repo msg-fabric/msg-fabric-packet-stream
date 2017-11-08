eb3df71de25f5ca5f9f7e751ed74fa1bfdfe96fd
Shane Holloway <shane.holloway@ieee.org>
2017-11-08 09:14:52 -0700

    Refactored rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,31 @@
 import babel from 'rollup-plugin-babel'
 
-const jsy_preset =  [ "jsy/lean", { no_stage_3: true, modules: false } ]
-const babel_plugin = babel({
-  babelrc: false,
-  presets: [ jsy_preset ],
-  plugins: [],
-  exclude: 'node_modules/**',
-})
+const externals = []
 
-const node_plugins = [babel_plugin]
-const browser_plugins = [babel_plugin]
+const plugins = [jsy_plugin()]
 
 export default [
-	{
-		input: 'code/index.cjs.js',
-		output: [
-			{ file: `dist/index.js`, format: 'cjs' },
-		],
-    plugins: node_plugins,
-	},
-	{
-		input: 'code/index.mjs',
-		output: [
-			{ file: `dist/index.mjs`, format: 'es' },
-		],
-    plugins: node_plugins,
-	},
-	{
-		input: 'code/dataview.js',
+	{ input: 'code/index.cjs.js',
+		output: [{ file: `dist/index.js`, format: 'cjs' }],
+    externals, plugins },
+
+	{ input: 'code/index.mjs',
+		output: [{ file: `dist/index.mjs`, format: 'es' }],
+    externals, plugins },
+
+	{ input: 'code/dataview.js',
     name: 'msg-fabric-packet-stream/dataview',
-		output: [
-			{ file: `dist/dataview.umd.js`, format: 'umd' },
-		],
-    plugins: browser_plugins,
-	},
+		output: [{ file: `dist/dataview.umd.js`, format: 'umd' }],
+    externals, plugins },
 ]
+
+
+
+
+function jsy_plugin() {
+  const jsy_preset = [ 'jsy/lean', { no_stage_3: true, modules: false } ]
+  return babel({
+    exclude: 'node_modules/**',
+    presets: [ jsy_preset ],
+    plugins: [],
+    babelrc: false }) }
